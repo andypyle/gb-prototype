77cdb87d331d2f2f3fbb92b4abb04eeb8ec8eaa5
Andrew Pyle <andy.pyle@gmail.com>
2015-05-31 19:19:16 -0700

    Navbar animation ready. Nav mouseovers working.

--- js/app.js
+++ js/app.js
@@ -1,9 +1,35 @@
 $(document).ready(function(){
+	var comingsoon = $('.bwg-coming-soon');
 	var scrollBtn = $('.scroller');
+	var navigation = $('#nav');
+
+	//navigation.hide();
+	comingsoon.hide();
+	comingsoon.velocity("fadeIn", { duration: 3500 });
+
+
+	$(window).scroll(function(){
+		var wScroll = $(this).scrollTop();
+		if(wScroll >= (navigation.offset().top - ($(this).height()))){
+			
+			navigation.css({
+				'opacity': wScroll * 0.0015
+			});
+			
+			navigation.height(wScroll / 4);
+			if(navigation.height() >= $('.brandname').outerHeight()){
+				navigation.height($('.brandname').outerHeight());
+			}
+		}
+		//console.log($('main').position().top);
+	});
+	
+
+	
 
 	scrollBtn.click(function(e){
 		e.preventDefault();
 		var getHref = $($(this).attr('href'));
 		getHref.velocity("scroll", 1100);
-	})
+	});
 });
